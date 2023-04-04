ddcd19d81be4c481b0b328331340b3565b24d91a
Pierfrancesco Oselin <pierfrancesco.oselin@volvocars.com>
2023-04-04 18:08:18 +0200

    new graphs

--- algorithms/BayesianOptimization.py
+++ algorithms/BayesianOptimization.py
@@ -1,10 +1,53 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.stats import qmc
 from sklearn.gaussian_process import GaussianProcessRegressor
 from typing import List
 from scipy.stats import norm
-import json, sys, argparse, ast
+import warnings
 
+
+def sobol_sampling(n_samples:int , dimension:int, boundaries):
+    """
+    Generate a Latin Hypercube Sample of size n and dimension d.
+
+    Parameters:
+    - n_samples (int): The number of samples to generate.
+    - dimension (int): The dimension of each sample.
+    - lower_bounds: List[float] N-dimensional array with the lower bounds values
+    - upper_bounds: List[float] N-dimensional array with the upper bounds values
+
+    Returns:
+    - numpy.ndarray: A n-by-d matrix of samples, where each row is a sample of length d.
+    """
+    boundaries = np.array(boundaries)
+
+    samples = qmc.Sobol(scramble=False, d=dimension)
+    samples = samples.random(n_samples, workers=-1)
+    samples = qmc.scale(samples, boundaries[0], boundaries[1])
+
+    return samples
+
+def latin_hypercube_sampling(n_samples:int , dimension:int, boundaries):
+    """
+    Generate a Latin Hypercube Sample of size n and dimension d.
+
+    Parameters:
+    - n_samples (int): The number of samples to generate.
+    - dimension (int): The dimension of each sample.
+    - lower_bounds: List[float] N-dimensional array with the lower bounds values
+    - upper_bounds: List[float] N-dimensional array with the upper bounds values
+
+    Returns:
+    - numpy.ndarray: A n-by-d matrix of samples, where each row is a sample of length d.
+    """
+
+    boundaries = np.array(boundaries)
+
+    samples = qmc.LatinHypercube(scramble=False,d=dimension)
+    samples = samples.random(n_samples)
+    samples = qmc.scale(samples, boundaries[0], boundaries[1])
+    
+    return samples
 
 def surrogate_model(model: GaussianProcessRegressor, X):
     prediction = model.predict(X.T, return_std=True)
@@ -21,15 +64,24 @@ def acquisition(model, X, samples):
     return probs
 
 
-
-def bayesian_optimization(f, dimension:int=2, n_samples:int=15, sampling_budget:int=100, boundaries=List[float]):
+def bayesian_optimization(f, dimension:int=2, n_samples:int=15, sampling_budget:int=100, sampling_method="sobol", boundaries=List[float]):
 
     # Modify the function to obtain the minimization
     fun = lambda x: (-1)*f(x)
 
     boundaries = np.array(boundaries)
     # Decide new samples to analyze
-    X =  np.random.uniform(low=boundaries[0], high=boundaries[1], size=[n_samples, dimension]).T
+    if (sampling_method == "sobol"):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            X = sobol_sampling(n_samples=n_samples, dimension=dimension, boundaries=boundaries).T
+    elif (sampling_method == "random"):
+        X = np.random.uniform(low=boundaries[0], high=boundaries[1], size=(n_samples, dimension)).T
+    elif (sampling_method == "latin_hypercube"):
+        X = latin_hypercube_sampling(n_samples=n_samples, dimension=dimension, boundaries=boundaries).T
+    else:
+        # Default choice
+        X = np.random.uniform(low=boundaries[0], high=boundaries[1], size=(n_samples, dimension)).T
 
     # Evaluate the function
     Y = fun(X)
@@ -57,7 +109,6 @@ def bayesian_optimization(f, dimension:int=2, n_samples:int=15, sampling_budget:
 
         # Find the actual value
         Y_new = fun(X_new)
-        #print("STATUS:", model.predict(X_new.T)[0])
         #est, _ = surrogate_model(model, X_new)
         #print(f"Estimation error:{Y_new - est}")
 
--- algorithms/ParticleSwarmOptimization.py
+++ algorithms/ParticleSwarmOptimization.py
@@ -1,10 +1,69 @@
 import numpy as np
-def particle_swarm(fun, dimension:int=2, pos_boundaries=None, vel_boundaries=None, sampling_budget:int=1000, n_particles=50, tol=10e-6, c1=0.1, c2=0.1, w=0.8):
+from scipy.stats import qmc
+import warnings
 
-    pos_boundaries = np.array(pos_boundaries)
+def sobol_sampling(n_samples:int , dimension:int, boundaries):
+    """
+    Generate a Latin Hypercube Sample of size n and dimension d.
+
+    Parameters:
+    - n_samples (int): The number of samples to generate.
+    - dimension (int): The dimension of each sample.
+    - lower_bounds: List[float] N-dimensional array with the lower bounds values
+    - upper_bounds: List[float] N-dimensional array with the upper bounds values
+
+    Returns:
+    - numpy.ndarray: A n-by-d matrix of samples, where each row is a sample of length d.
+    """
+    boundaries = np.array(boundaries)
+
+    samples = qmc.Sobol(scramble=False, d=dimension)
+    samples = samples.random(n_samples, workers=-1)
+    samples = qmc.scale(samples, boundaries[0], boundaries[1])
+
+    return samples
+
+def latin_hypercube_sampling(n_samples:int , dimension:int, boundaries):
+    """
+    Generate a Latin Hypercube Sample of size n and dimension d.
+
+    Parameters:
+    - n_samples (int): The number of samples to generate.
+    - dimension (int): The dimension of each sample.
+    - lower_bounds: List[float] N-dimensional array with the lower bounds values
+    - upper_bounds: List[float] N-dimensional array with the upper bounds values
+
+    Returns:
+    - numpy.ndarray: A n-by-d matrix of samples, where each row is a sample of length d.
+    """
+
+    boundaries = np.array(boundaries)
+
+    samples = qmc.LatinHypercube(scramble=False,d=dimension)
+    samples = samples.random(n_samples)
+    samples = qmc.scale(samples, boundaries[0], boundaries[1])
+    
+    return samples
+
+
+def particle_swarm(fun, dimension:int=2, boundaries=None, vel_boundaries=None, sampling_budget:int=1000, 
+                   sampling_method="sobol", n_particles=50, tollerance=10e-6, c1=0.1, c2=0.1, w=0.8):
+
+    boundaries = np.array(boundaries)
     vel_boundaries = np.array(vel_boundaries)
     
-    X = np.random.uniform(low=pos_boundaries[0], high=pos_boundaries[1], size=(n_particles, dimension)).T
+    if (sampling_method == "sobol"):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            X = sobol_sampling(n_samples=n_particles, dimension=dimension, boundaries=boundaries).T + np.random.uniform(low=-0.2, high=0.2, size=(n_particles, dimension)).T
+    elif (sampling_method == "random"):
+        X = np.random.uniform(low=boundaries[0], high=boundaries[1], size=(n_particles, dimension)).T
+    elif (sampling_method == "latin_hypercube"):
+        X = latin_hypercube_sampling(n_samples=n_particles, dimension=dimension, boundaries=boundaries).T + np.random.uniform(low=-0.2, high=0.2, size=(n_particles, dimension)).T
+    else:
+        # Default choice
+        X = np.random.uniform(low=boundaries[0], high=boundaries[1], size=(n_particles, dimension)).T
+
     V = np.random.uniform(low=vel_boundaries[0], high=vel_boundaries[1], size=(n_particles, dimension)).T
 
     # Initialize data for the first iteration
@@ -48,11 +107,10 @@ def particle_swarm(fun, dimension:int=2, pos_boundaries=None, vel_boundaries=Non
         X_log = np.hstack((X_log, gbest.copy()))
         Y_log = np.hstack((Y_log, gbest_obj.copy()))
 
-        # if (np.abs(Y_log[-1] - Y_log[-2]) < tol):
+        # if (np.abs(Y_log[-1] - Y_log[-2]) < tollerance):
         #     print(f"Tollearnce reacehed, {iteration} iterations is enough.")
         #     break
 
     X_best, Y_best = X_log[:, -1], Y_log[-1]
 
     return X_best, Y_best, X_log.T, Y_log.T
-
